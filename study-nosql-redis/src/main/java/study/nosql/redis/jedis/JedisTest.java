71f530121f2c96fb54f2e817614266c937b6ea33
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-04-01 19:40:43 +0800

    更新代码

--- study-nosql-redis/src/main/java/study/nosql/redis/jedis/JedisTest.java
+++ study-nosql-redis/src/main/java/study/nosql/redis/jedis/JedisTest.java
@@ -7,7 +7,7 @@ public class JedisTest {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("114.67.102.8", 7379);
         jedis.set("Jedis:key:123", "Hello Work!");
-        System.out.println(jedis.get("Jedis:key:123JedisTest"));
+        System.out.println(jedis.get("Jedis:key:123"));
         jedis.close();
     }
 }
